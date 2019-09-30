db8d24c35e0fa4eb8b8fa9160d374a034046ca50
Slobodan Vlaic <slobiiv@gmail.com>
2019-10-01 00:54:42 +0100

    Introduction to JavaScript

--- Part two(2nd Month)/Javascript/01.Introduction-to-JavaScript.js
+++ Part two(2nd Month)/Javascript/01.Introduction-to-JavaScript.js
@@ -1,1 +1,3 @@
 /* Just like learning any language; with enough time in practice, you can become fluent. */
+
+/* Don't just watch the videos in this section and fly through the content. Really work on the exercises */
---
+++ Part two(2nd Month)/Javascript/02.What-Is-Javascript.js
@@ -0,0 +1,12 @@
+/* 
+- JavaScript was created in 1995 by the Netscape web browser.
+  It was a way to add actions to websites.
+  * Think of JavaScript as the verb in anything you do on a website. You tweet, you play a YouTube video, you sign up for a user name.
+- Instagram use JavaScript
+- VR games
+- Robotics
+
+* What is JavaScript really? It's a file, that can write instructions to a computer.
+
+* Everything is built one block at a time. One simple logic at a time. 
+*/
