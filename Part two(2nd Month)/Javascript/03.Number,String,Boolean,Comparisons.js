54685323421f9e873525bde4d35329bd35b0ed0e
Slobodan Vlaic <slobiiv@gmail.com>
2019-10-01 01:11:19 +0100

    Numbers, Strings, Booleans, Comparisons

---
+++ Part two(2nd Month)/Javascript/03.Number,String,Boolean,Comparisons.js
@@ -0,0 +1,27 @@
+/* Using Numbers in console in the browser */
+3+4
+4-5
+9+4
+(3+4) * 2
+12 % 4 //remainder 0
+12 % 5 //remainder 2
+
+/* Using Strings in console in the browser */
+'Bob'
+'Andrei'
+'Hello' + ' There' // Hello There
+'This isn\'t very nice' // ("This isn't very nice") \ignores the quote
+
+/* Adding Numbers to Strings (Keep numbers with numbers) */
+10 + '34' // 1034
+10 - '3' // 7
+'hello' - 'bye' // NaN - Not a Number
+
+/* Boolean - TRUE and FALSE */
+3 > 2 // true
+5 > 10 // false
+5 >= 5 // true
+5 <= 5 // true
+3 === 3 // true
+3 !== 3 // false (3 Doesn't equal 3)
+4 !== 5 // true
