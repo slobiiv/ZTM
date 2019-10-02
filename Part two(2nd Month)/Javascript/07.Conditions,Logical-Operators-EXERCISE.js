725f72f74f057738c0232c2c8f9c6a2dc40c0067
Slobodan Vlaic <slobiiv@gmail.com>
2019-10-02 22:31:06 +0100

    Conditions and Logical Operator exercise DONE!

--- Part two(2nd Month)/Javascript/07.Conditions,Logical-Operators-EXERCISE.js
+++ Part two(2nd Month)/Javascript/07.Conditions,Logical-Operators-EXERCISE.js
@@ -11,3 +11,13 @@
 
 // IF they say they are over 18, respond with:
 // "Powering On. Enjoy the ride!"
+
+var age = prompt('How old are you?');
+    age = Number(age);
+if(age < 18) {
+  alert('Sorry, you are too young to drive this car. Powering off!');
+} else if(age === 18) {
+  alert('Congratulations on your first year of driving. Enjoy the ride!');
+} else if(age > 18) {
+  alert('Powering On. Enjoy the ride!');
+}
