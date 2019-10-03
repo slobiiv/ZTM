34941c1876418491bab16cb1e77dffcb28333ed5
Slobodan Vlaic <slobiiv@gmail.com>
2019-10-04 00:01:23 +0100

    commit

--- Part two(2nd Month)/Javascript/10.Functions-EXERCISE.js
+++ Part two(2nd Month)/Javascript/10.Functions-EXERCISE.js
@@ -12,16 +12,9 @@ if (Number(age) < 18) {
 	alert("Congratulations on your first year of driving. Enjoy the ride!");
 }
 
+
 //1. Make the above code have a function called checkDriverAge(). Whenever you call this function, you will get prompted for age. Use Function Declaration to create this function.
-// Notice the benefit in having checkDriverAge() instead of copying and pasting the function everytime?
-
-//2. Create another function that does the same thing, called checkDriverAge2() using Function Expression.
-
-//BONUS: Instead of using the prompt. Now, only use console.log and make the checkDriverAge() function accept an argument of age, so that if you enter:
-checkDriverAge(92);
-// it returns "Powering On. Enjoy the ride!"
-
-// 1.
+// Notice the benefit in having checkDriverAge() instead of copying and pasting the function every time?
 function checkDriverAge() {
   var age = prompt('What is your age?');
   if (Number(age) < 18) {
@@ -34,7 +27,7 @@ function checkDriverAge() {
 }
 checkDriverAge();
 
-// 2. 
+//2. Create another function that does the same thing, called checkDriverAge2() using Function Expression. 
 var checkDriverAge2 = function() {
   var age = prompt('What is your age?');
   if (Number(age) < 18) {
@@ -47,7 +40,9 @@ var checkDriverAge2 = function() {
 }
 checkDriverAge2();
 
-// 3.
+//BONUS: Instead of using the prompt. Now, only use console.log and make the checkDriverAge() function accept an argument of age, so that if you enter:
+// checkDriverAge(92);
+// it returns "Powering On. Enjoy the ride!"
 function checkDriversAge(age) {
   if (Number(age) < 18) {
     console.log("Sorry, you are too yound to drive this car. Powering off");
@@ -57,4 +52,4 @@ function checkDriversAge(age) {
     console.log("Congratulations on your first year of driving. Enjoy the ride!");
   }
 }
-checkDriversAge(22);
+checkDriversAge(92);
