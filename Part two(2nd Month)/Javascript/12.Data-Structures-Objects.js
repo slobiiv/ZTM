00c0062343b8e8a4568e61c5422b46597c2244aa
Slobodan Vlaic <slobiiv@gmail.com>
2019-10-05 23:16:56 +0100

    Objects

---
+++ Part two(2nd Month)/Javascript/12.Data-Structures-Objects.js
@@ -0,0 +1,12 @@
+/* Objects are collections of property */
+var user = {
+  name: 'John',
+  age: 34,
+  hobby: 'Soccer',
+  isMarried: false
+}
+console.log(user.name); // 'John'
+
+/* How can we add (change) properties to an object? */
+user.favoriteFood = 'spinach';
+user.isMarried = true;
