a19ec5ce18c6cd1d0c60eef2037fd793baa6f47e
Slobodan Vlaic <slobiiv@gmail.com>
2019-10-06 00:05:04 +0100

    Facebook app :)

---
+++ Part two(2nd Month)/Javascript/13.Objects-and-Arrays-For-Facebook-App-EXERCISE.js
@@ -0,0 +1,26 @@
+// Create an object and an array which we will use in our facebook exercise. 
+
+// 1. Create an object that has properties "username" and "password". Fill those values in with strings.
+  var myUser = {
+    username: 'Heisenberg',
+    password: 'JessIsMyBestFriend'
+  }
+
+// 2. Create an array which contains the object you have made above and name the array "database".
+  var database = [myUser];
+  
+// 3. Create an array called "newsfeed" which contains 3 objects with properties "username" and "timeline".
+  var newsfeed = [
+    {
+      username:'Dan88',
+      timeline: 'When nothing goes right, go left.'
+    },
+    {
+      username:'Sane79',
+      timeline: 'Keep your heels, head, and standards high.'
+    },
+    {
+      username:'Relja19',
+      timeline: 'Be yourself; everyone else is already taken.'
+    },
+  ];
