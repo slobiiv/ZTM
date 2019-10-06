727a8d5024316ba1c458beaee8cee1231f61270a
Slobodan Vlaic <slobiiv@gmail.com>
2019-10-06 02:14:38 +0100

    Sign in Facebook login exercise

--- Part two(2nd Month)/Javascript/13.Objects-and-Arrays-Build-Facebook-App-EXERCISE.js
+++ Part two(2nd Month)/Javascript/13.Objects-and-Arrays-Build-Facebook-App-EXERCISE.js
@@ -1,13 +1,18 @@
 // Create an object and an array which we will use in our facebook exercise. 
 
 // 1. Create an object that has properties "username" and "password". Fill those values in with strings.
-  var myUser = {
-    username: 'Heisenberg',
-    password: 'JessIsMyBestFriend'
-  }
+//    {
+//     username: 'Heisenberg',
+//     password: 'JessIsMyBestFriend'
+//    }
 
 // 2. Create an array which contains the object you have made above and name the array "database".
-  var database = [myUser];
+  var database = [
+    {
+      username: 'Heisenberg',
+      password: 'JessIsMyBestFriend'
+    }
+  ];
   
 // 3. Create an array called "newsfeed" which contains 3 objects with properties "username" and "timeline".
   var newsfeed = [
@@ -24,3 +29,17 @@
       timeline: 'Be yourself; everyone else is already taken.'
     },
   ];
+
+  
+  var userNamePrompt = prompt('What is your username?');
+  var passwordPrompt = prompt('What is your password?');
+
+  function signIn(username, password) {
+    if(username === database[0].username  && password === database[0].password ) {
+      alert('You are logged in!')
+      console.log(newsfeed);
+    } else {
+      alert('Your details are incorrect!');
+    }
+  }
+  signIn(userNamePrompt,passwordPrompt);
