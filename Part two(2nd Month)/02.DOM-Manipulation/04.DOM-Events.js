301f2b3b4e5a435385bac7449ad240fd4b8e8be9
Slobodan Vlaic <slobiiv@gmail.com>
2019-10-10 23:16:29 +0100

    added a Note about callback functions

--- Part two(2nd Month)/02.DOM-Manipulation/04.DOM-Events.js
+++ Part two(2nd Month)/02.DOM-Manipulation/04.DOM-Events.js
@@ -58,3 +58,24 @@ function addListAfterKeyPress(event) {
 input.addEventListener('keypress',addListAfterKeyPress);
 
 
+
+/* NOTE: Callback Functions
+
+In the previous video you saw something interesting:
+Event listener syntax : 
+
+    button.addEventListener("click", addListAfterClick);
+    input.addEventListener("keypress", addListAfterKeypress);
+
+You didn't see the function being called like this as you may have expected: 
+
+    button.addEventListener("click", addListAfterClick());
+    input.addEventListener("keypress", addListAfterKeypress(event));
+
+This is something called a callback function. 
+When that line of javascript runs, we don't want the addLisAfterClick function to run 
+because we are just adding the event listener now to wait for click or keypress. 
+We want to let it know though that we want this action to happen when a click happens. 
+So the function now automatically gets run (gets added the ()) every time the click happens. 
+So we are passing a reference to the function without running it. 
+*/
