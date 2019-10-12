13f049c9d0922305fa7b71d96a9c2913ce3cd71f
Slobodan Vlaic <slobiiv@gmail.com>
2019-10-12 23:00:58 +0100

    Doing again same exercise, practice

---
+++ Part two(2nd Month)/02.DOM-Manipulation/05.DOM-Exercise/myTodo/main.js
@@ -0,0 +1,32 @@
+var input = document.querySelector('input');
+var addBtn = document.getElementById('addBtn');
+var deleteBtn = document.getElementsByClassName('delete');
+var ul = document.getElementById('tasks');
+
+addBtn.addEventListener('click', function() {
+  if(input.value.length > 0) {
+    var li = document.createElement('li');
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+  
+    input.value = "";
+
+    deleteBtn = document.createElement('button');
+    deleteBtn.appendChild(document.createTextNode('delete'));
+    li.appendChild(deleteBtn);
+  }
+})
+
+input.addEventListener('keypress', function(event) {
+  if(input.value.length > 0 && event.keyCode === 13) {
+    var li = document.createElement('li');
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+  
+    input.value = "";
+
+    deleteBtn = document.createElement('button');
+    deleteBtn.appendChild(document.createTextNode('delete'));
+    li.appendChild(deleteBtn);
+  }
+})
