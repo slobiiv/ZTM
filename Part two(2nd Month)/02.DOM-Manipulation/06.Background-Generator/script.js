f50f516bf5a9556fb4289ad5f0b1cb4713b464d9
Slobodan Vlaic <slobiiv@gmail.com>
2019-10-14 23:11:45 +0100

    background gradient JS exercise

---
+++ Part two(2nd Month)/02.DOM-Manipulation/06.Background-Generator/script.js
@@ -0,0 +1,8 @@
+/* 
+So we want to read what the value what the colors are of both inputs.
+And we want to put out a piece of text at the bottom here with those values in the CSS format.
+We'll need to select two inputs and then also select the h3 so we can enter CSS text here.  
+*/
+var css = document.querySelector('h3');
+var color1 = document.querySelector('.color1');
+var color2 = document.querySelector('.color2');
