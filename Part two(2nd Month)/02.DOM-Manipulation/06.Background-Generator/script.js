e8c1f52c37d5ca5a5bc5ea2cfddee610104a386d
Slobodan Vlaic <slobiiv@gmail.com>
2019-10-15 22:02:21 +0100

    Fundamentals V - We want to minimize the amount of DOM manipulation and events.

--- Part two(2nd Month)/02.DOM-Manipulation/06.Background-Generator/script.js
+++ Part two(2nd Month)/02.DOM-Manipulation/06.Background-Generator/script.js
@@ -6,3 +6,29 @@ We'll need to select two inputs and then also select the h3 so we can enter CSS 
 var css = document.querySelector('h3');
 var color1 = document.querySelector('.color1');
 var color2 = document.querySelector('.color2');
+var body = document.getElementById('gradient');
+// body.style.background = 'red';
+
+function setGradient() {
+  body.style.background = 
+  'linear-gradient(to right, '
+  + color1.value 
+  + ', ' 
+  + color2.value 
+  + ')'; 
+
+  css.textContent = body.style.background + ';';
+}
+/* input Event, whenever the input value changes it kicks in */
+color1.addEventListener('input', setGradient);
+color2.addEventListener('input', setGradient);
+
+/* 
+A few things you can try:
+
+1. Write code so that the colour inputs match the background generated on the first page load. 
+
+2. Display the initial CSS linear gradient property on page load.
+
+3. BONUS: Add a random button which generates two random numbers for the colour inputs.
+*/
---
+++ Part two(2nd Month)/02.DOM-Manipulation/07.Developer-Fundamentals-V.js
@@ -0,0 +1,3 @@
+/* FUNDAMENTALS: ACCESSING THE DOM  */
+// You want to minimize the amount of DOM manipulation and events.
+// It's a very important concept in building Web apps, and you also want to be smart and use the best methods to minimize this.
